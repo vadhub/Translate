ce2f7fcd9cbcb01c8794de41e09db990fed05536
Vadim <gabderahmanov99@gmail.com>
2019-07-01 14:40:45 +0400

    add lib for translator

--- Translate/src/com/vg/starter/Starter.java
+++ Translate/src/com/vg/starter/Starter.java
@@ -1,10 +1,15 @@
 package com.vg.starter;
 
+import com.gtranslate.Language;
+import com.gtranslate.Translator;
+
 public class Starter {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Translator translate = Translator.getInstance();
+		
+		String text = translate.translate("Hello world", Language.ENGLISH, Language.AFRIKAANS);
+		System.out.println(text);
 	}
 
 }
