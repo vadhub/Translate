c00062b0cd5e0108d3605fed28a81322d4546d3e
Vadim <gabderahmanov99@gmail.com>
2019-07-02 14:28:19 +0400

    add traslate class

--- Translate/src/com/vg/starter/Starter.java
+++ Translate/src/com/vg/starter/Starter.java
@@ -1,15 +1,13 @@
 package com.vg.starter;
 
-import com.gtranslate.Language;
-import com.gtranslate.Translator;
+import com.vg.translate.Traslater;
 
 public class Starter {
 
 	public static void main(String[] args) {
-		Translator translate = Translator.getInstance();
+		Traslater tr = new Traslater();
+		tr.translateToRus("Hello");
 		
-		String text = translate.translate("Hello world", Language.ENGLISH, Language.AFRIKAANS);
-		System.out.println(text);
 	}
 
 }
